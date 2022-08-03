eae17f380a15345617f2a21b3e79db545000a38c
Ishaan Rao <ishaanrao@Ishaans-MacBook-Air.local>
2022-08-02 20:44:26 -0400

    Created the repeater() function and added the contents

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ def wordcounter(inp):
             spaces += 1
     print(f"There are {spaces + 1} words in this sentence.")
 
-def repeater(inp):
-    pass
+def repeater(inp,search):
+    search_in_x = 0
+    wordlist = []
+    for x in inp:
+        if search in userin:
+           search_in_x += 1
+        
+    print(f"{search} appears {search_in_x} times in the inputted string.")
 
 
 
@@ -18,3 +24,11 @@ userin = input("Input a string to be classified: ")
 
 # Take user input and add it as a parameter to the word counting function
 wordcounter(userin)
+
+# Ask user for search word
+searchbool = input("Would you like to search for a word? ")
+if searchbool == "yes" or "Yes":
+    searchword = input("What word? ")
+else: print("Goodbye!")
+
+repeater(userin,searchword)
